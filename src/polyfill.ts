9b2db3480972b7a305568535f5fb747280c9c22b
Robert Coe <chakrawc@gmail.com>
2022-01-07 11:27:41 -0800

    let's try this?

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,6 +1,9 @@
-import zlib from "https://cdn.esm.sh/v61/browserify-zlib@0.2.0/deno/browserify-zlib.bundle.js";
-// Old polyfill doesn't conform to current Node.js spec
-Object.assign(zlib, { constants: { Z_SYNC_FLUSH: zlib.Z_SYNC_FLUSH } });
+if (!Deno.stderr) {
+  Object.assign(Deno, "stderr", { value: new WritableStream() });
+}
+if (!Deno.stdout) {
+  Object.assign(Deno, "stdout", { value: new WritableStream() });
+}
 
 // Used for inquire
 // globalThis.eval = () => {};
@@ -15,3 +18,7 @@ setTimeout = (cb: () => void, delay: number): number => {
   }
   return oldSetTimeout(cb, delay);
 };
+
+import zlib from "https://cdn.esm.sh/v61/browserify-zlib@0.2.0/deno/browserify-zlib.bundle.js";
+// Old polyfill doesn't conform to current Node.js spec
+Object.assign(zlib, { constants: { Z_SYNC_FLUSH: zlib.Z_SYNC_FLUSH } });
